c468c6aadbb7ef290677ae50a3a6bf07f8e9d682
Huansong Fu <fuhuansong@gmail.com>
2023-03-20 07:51:33 -0700

    Generate gp_ view for desired pg_ system views
    For a selected list of PG system views (started with 'pg_'prefix ), we
    will create a corresponding 'gp_' view for each one in the list.
    Each 'gp_' view is basically a UNION ALL of the results of running the
    corresponding 'pg_' view on all segments (including the coordinator).
    
    Note that, these views do not aggregate the results. The aggregate
    version of the views will be named with a '_summary' appendix (such
    as 'gp_stat_all_tables_summary').
    
    To add a new 'pg_' view to this list, simply put the name in file
    'src/backend/catalog/system_views_gp.in'. This commit adds an initial
    list of views that we think make sense to have 'gp_' views.
    
    With this change, we also remove the existing definition of
    gp_stat_archiver view and let it be generated automatically.
    We also had gp_stat_replication but it carries additional column than
    pg_stat_replication so it cannot use the automatic way.

--- src/bin/initdb/initdb.c
+++ src/bin/initdb/initdb.c
@@ -175,6 +175,7 @@ static char *external_fts_files;
 #endif
 static char *system_functions_file;
 static char *system_views_file;
+static char *system_views_gp_file;
 static bool success = false;
 static bool made_new_pgdata = false;
 static bool found_existing_pgdata = false;
@@ -2833,6 +2834,7 @@ setup_data_file_paths(void)
 	set_input(&system_constraints_file, "system_constraints.sql");
 	set_input(&system_functions_file, "system_functions.sql");
 	set_input(&system_views_file, "system_views.sql");
+	set_input(&system_views_gp_file, "system_views_gp.sql");
 
 	set_input(&cdb_init_d_dir, "cdb_init.d");
 
@@ -2866,6 +2868,7 @@ setup_data_file_paths(void)
 #endif
 	check_input(system_functions_file);
 	check_input(system_views_file);
+	check_input(system_views_gp_file);
 }
 
 
@@ -3233,6 +3236,7 @@ initialize_data_directory(void)
 	 */
 
 	setup_run_file(cmdfd, system_views_file);
+	setup_run_file(cmdfd, system_views_gp_file);
 
 	setup_description(cmdfd);
 
