6a9651524069ecb386f4db1cd2acdd6fbf6c6b16
Maxim Smyatkin <smyatkinmaxim@gmail.com>
2023-04-06 13:24:25 +0300

    [yagp_hooks_collector] Apply llvm code style

--- src/GrpcConnector.cpp
+++ src/GrpcConnector.cpp
@@ -5,51 +5,43 @@
 #include <grpcpp/channel.h>
 #include <string>
 
-class GrpcConnector::Impl
-{
+class GrpcConnector::Impl {
 public:
-    Impl()
-    {
-        GOOGLE_PROTOBUF_VERIFY_VERSION;
-        this->stub = yagpcc::SetQueryInfo::NewStub(grpc::CreateChannel(
-            SOCKET_FILE, grpc::InsecureChannelCredentials()));
+  Impl() {
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+    this->stub = yagpcc::SetQueryInfo::NewStub(
+        grpc::CreateChannel(SOCKET_FILE, grpc::InsecureChannelCredentials()));
+  }
+
+  yagpcc::MetricResponse set_metric_query(yagpcc::SetQueryReq req) {
+    yagpcc::MetricResponse response;
+    grpc::ClientContext context;
+    auto deadline =
+        std::chrono::system_clock::now() + std::chrono::milliseconds(50);
+    context.set_deadline(deadline);
+
+    grpc::Status status = (stub->SetMetricQuery)(&context, req, &response);
+
+    if (!status.ok()) {
+      response.set_error_text("Connection lost: " + status.error_message() +
+                              "; " + status.error_details());
+      response.set_error_code(yagpcc::METRIC_RESPONSE_STATUS_CODE_ERROR);
     }
 
-    yagpcc::MetricResponse set_metric_query(yagpcc::SetQueryReq req)
-    {
-        yagpcc::MetricResponse response;
-        grpc::ClientContext context;
-        auto deadline = std::chrono::system_clock::now() + std::chrono::milliseconds(50);
-        context.set_deadline(deadline);
-
-        grpc::Status status = (stub->SetMetricQuery)(&context, req, &response);
-
-        if (!status.ok())
-        {
-            response.set_error_text("Connection lost: " + status.error_message() + "; " + status.error_details());
-            response.set_error_code(yagpcc::METRIC_RESPONSE_STATUS_CODE_ERROR);
-        }
-
-        return response;
-    }
+    return response;
+  }
 
 private:
-    const std::string SOCKET_FILE = "unix:///tmp/yagpcc_agent.sock";
-    const std::string TCP_ADDRESS = "127.0.0.1:1432";
-    std::unique_ptr<yagpcc::SetQueryInfo::Stub> stub;
+  const std::string SOCKET_FILE = "unix:///tmp/yagpcc_agent.sock";
+  const std::string TCP_ADDRESS = "127.0.0.1:1432";
+  std::unique_ptr<yagpcc::SetQueryInfo::Stub> stub;
 };
 
-GrpcConnector::GrpcConnector()
-{
-    impl = new Impl();
-}
+GrpcConnector::GrpcConnector() { impl = new Impl(); }
 
-GrpcConnector::~GrpcConnector()
-{
-    delete impl;
-}
+GrpcConnector::~GrpcConnector() { delete impl; }
 
-yagpcc::MetricResponse GrpcConnector::set_metric_query(yagpcc::SetQueryReq req)
-{
-    return impl->set_metric_query(req);
+yagpcc::MetricResponse
+GrpcConnector::set_metric_query(yagpcc::SetQueryReq req) {
+  return impl->set_metric_query(req);
 }
