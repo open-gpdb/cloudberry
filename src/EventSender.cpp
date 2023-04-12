0e3552252f893c46b4ffe148e5633db9b698435c
Maxim Smyatkin <smyatkinmaxim@gmail.com>
2023-04-12 12:37:29 +0300

    Fix two segfaults
    1. Initialize query instrumentation to NULL so that it can be properly
    checked later (temporary solution, need to find a proper fix)
    2. Don't collect spillinfo on query end. Reason: a) it will always be
    zero and b) it could crash if we failed to enlarge a spillfile. Seems
    like we need some cummulative statistics for spillinfo. Need to check
    what explain analyze use.

--- src/EventSender.cpp
+++ src/EventSender.cpp
@@ -4,19 +4,20 @@
 
 extern "C" {
 #include "postgres.h"
+
 #include "access/hash.h"
-#include "utils/metrics_utils.h"
-#include "utils/elog.h"
-#include "executor/executor.h"
-#include "commands/explain.h"
 #include "commands/dbcommands.h"
+#include "commands/explain.h"
 #include "commands/resgroupcmds.h"
+#include "executor/executor.h"
+#include "utils/elog.h"
+#include "utils/metrics_utils.h"
 
-#include "cdb/cdbvars.h"
 #include "cdb/cdbexplain.h"
+#include "cdb/cdbvars.h"
 
-#include "tcop/utility.h"
 #include "stat_statements_parser/pg_stat_statements_ya_parser.h"
+#include "tcop/utility.h"
 
 void get_spill_info(int ssid, int ccid, int32_t *file_count,
                     int64_t *total_bytes);
@@ -120,35 +121,41 @@ void set_query_info(yagpcc::QueryInfo *qi, QueryDesc *query_desc,
 void set_metric_instrumentation(yagpcc::MetricInstrumentation *metrics,
                                 QueryDesc *query_desc) {
   auto instrument = query_desc->planstate->instrument;
-  metrics->set_ntuples(instrument->ntuples);
-  metrics->set_nloops(instrument->nloops);
-  metrics->set_tuplecount(instrument->tuplecount);
-  metrics->set_firsttuple(instrument->firsttuple);
-  metrics->set_startup(instrument->startup);
-  metrics->set_total(instrument->total);
-  auto &buffusage = instrument->bufusage;
-  metrics->set_shared_blks_hit(buffusage.shared_blks_hit);
-  metrics->set_shared_blks_read(buffusage.shared_blks_read);
-  metrics->set_shared_blks_dirtied(buffusage.shared_blks_dirtied);
-  metrics->set_shared_blks_written(buffusage.shared_blks_written);
-  metrics->set_local_blks_hit(buffusage.local_blks_hit);
-  metrics->set_local_blks_read(buffusage.local_blks_read);
-  metrics->set_local_blks_dirtied(buffusage.local_blks_dirtied);
-  metrics->set_local_blks_written(buffusage.local_blks_written);
-  metrics->set_temp_blks_read(buffusage.temp_blks_read);
-  metrics->set_temp_blks_written(buffusage.temp_blks_written);
-  metrics->set_blk_read_time(INSTR_TIME_GET_DOUBLE(buffusage.blk_read_time));
-  metrics->set_blk_write_time(INSTR_TIME_GET_DOUBLE(buffusage.blk_write_time));
+  if (instrument) {
+    metrics->set_ntuples(instrument->ntuples);
+    metrics->set_nloops(instrument->nloops);
+    metrics->set_tuplecount(instrument->tuplecount);
+    metrics->set_firsttuple(instrument->firsttuple);
+    metrics->set_startup(instrument->startup);
+    metrics->set_total(instrument->total);
+    auto &buffusage = instrument->bufusage;
+    metrics->set_shared_blks_hit(buffusage.shared_blks_hit);
+    metrics->set_shared_blks_read(buffusage.shared_blks_read);
+    metrics->set_shared_blks_dirtied(buffusage.shared_blks_dirtied);
+    metrics->set_shared_blks_written(buffusage.shared_blks_written);
+    metrics->set_local_blks_hit(buffusage.local_blks_hit);
+    metrics->set_local_blks_read(buffusage.local_blks_read);
+    metrics->set_local_blks_dirtied(buffusage.local_blks_dirtied);
+    metrics->set_local_blks_written(buffusage.local_blks_written);
+    metrics->set_temp_blks_read(buffusage.temp_blks_read);
+    metrics->set_temp_blks_written(buffusage.temp_blks_written);
+    metrics->set_blk_read_time(INSTR_TIME_GET_DOUBLE(buffusage.blk_read_time));
+    metrics->set_blk_write_time(
+        INSTR_TIME_GET_DOUBLE(buffusage.blk_write_time));
+  }
 }
 
-void set_gp_metrics(yagpcc::GPMetrics *metrics, QueryDesc *query_desc) {
-  int32_t n_spill_files = 0;
-  int64_t n_spill_bytes = 0;
-  get_spill_info(gp_session_id, gp_command_count, &n_spill_files,
-                 &n_spill_bytes);
-  metrics->mutable_spill()->set_filecount(n_spill_files);
-  metrics->mutable_spill()->set_totalbytes(n_spill_bytes);
-  if (query_desc->planstate->instrument) {
+void set_gp_metrics(yagpcc::GPMetrics *metrics, QueryDesc *query_desc,
+                    bool need_spillinfo) {
+  if (need_spillinfo) {
+    int32_t n_spill_files = 0;
+    int64_t n_spill_bytes = 0;
+    get_spill_info(gp_session_id, gp_command_count, &n_spill_files,
+                   &n_spill_bytes);
+    metrics->mutable_spill()->set_filecount(n_spill_files);
+    metrics->mutable_spill()->set_totalbytes(n_spill_bytes);
+  }
+  if (query_desc->planstate && query_desc->planstate->instrument) {
     set_metric_instrumentation(metrics->mutable_instrumentation(), query_desc);
   }
   fill_self_stats(metrics->mutable_systemstat());
@@ -200,6 +207,9 @@ void EventSender::query_metrics_collect(QueryMetricsStatus status, void *arg) {
 
 void EventSender::executor_after_start(QueryDesc *query_desc, int /* eflags*/) {
   elog(DEBUG1, "Query %s started event recording", query_desc->sourceText);
+  if (query_desc->planstate) {
+    query_desc->planstate->instrument = nullptr;
+  }
   auto req = get_query_req(query_desc, yagpcc::QueryStatus::QUERY_STATUS_START);
   set_query_info(req.mutable_query_info(), query_desc, false, true);
   send_query_info(&req, "started");
@@ -219,11 +229,15 @@ void EventSender::collect_query_submit(QueryDesc *query_desc) {
 
 void EventSender::collect_query_done(QueryDesc *query_desc,
                                      const std::string &status) {
-  elog(DEBUG1, "Query %s %s event recording", query_desc->sourceText,
+  elog(DEBUG1, "Query %s %s event recording",
+       query_desc->sourceText ? query_desc->sourceText : "<UNKNOWN>",
        status.c_str());
   auto req = get_query_req(query_desc, yagpcc::QueryStatus::QUERY_STATUS_DONE);
   set_query_info(req.mutable_query_info(), query_desc, false, false);
-  set_gp_metrics(req.mutable_query_metrics(), query_desc);
+  // NOTE: there are no cummulative spillinfo stats AFAIU, so no need to gather
+  // it here. It only makes sense when doing regular stat checks.
+  set_gp_metrics(req.mutable_query_metrics(), query_desc,
+                 /*need_spillinfo*/ false);
   send_query_info(&req, status);
 }
 
