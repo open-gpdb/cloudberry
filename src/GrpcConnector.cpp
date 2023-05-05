bb6aed426b3dec6bf1fcda22d8bb463aa26b4d9a
Maxim Smyatkin <smyatkinmaxim@gmail.com>
2023-05-05 12:46:56 +0300

    Change GRPC failure handling
    1) Give higher gRPC timeouts to query dispatcher as losing messages
    there is more critical
    2) If we've failed to send a message via gRPC we notify a background
    thread about it and refuse sending any new message until this thread
    re-establishes the lost connection

--- src/GrpcConnector.cpp
+++ src/GrpcConnector.cpp
@@ -4,30 +4,54 @@
 #include <grpc++/grpc++.h>
 #include <grpc++/channel.h>
 #include <string>
+#include <atomic>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+extern "C" {
+#include "postgres.h"
+#include "cdb/cdbvars.h"
+}
 
 class GrpcConnector::Impl {
 public:
   Impl() {
     GOOGLE_PROTOBUF_VERIFY_VERSION;
-    this->stub = yagpcc::SetQueryInfo::NewStub(
-        grpc::CreateChannel(SOCKET_FILE, grpc::InsecureChannelCredentials()));
+    channel =
+        grpc::CreateChannel(SOCKET_FILE, grpc::InsecureChannelCredentials());
+    stub = yagpcc::SetQueryInfo::NewStub(channel);
+    connected = true;
+    done = false;
+    reconnect_thread = std::thread(&Impl::reconnect, this);
+  }
+
+  ~Impl() {
+    done = true;
+    cv.notify_one();
+    reconnect_thread.join();
   }
 
   yagpcc::MetricResponse set_metric_query(yagpcc::SetQueryReq req) {
     yagpcc::MetricResponse response;
+    if (!connected) {
+      response.set_error_code(yagpcc::METRIC_RESPONSE_STATUS_CODE_ERROR);
+      response.set_error_text(
+          "Not tracing this query connection to agent has been lost");
+      return response;
+    }
     grpc::ClientContext context;
-    // TODO: find a more secure way to send messages than relying on a fixed
-    // timeout
+    int timeout = Gp_role == GP_ROLE_DISPATCH ? 500 : 250;
     auto deadline =
-        std::chrono::system_clock::now() + std::chrono::milliseconds(200);
+        std::chrono::system_clock::now() + std::chrono::milliseconds(timeout);
     context.set_deadline(deadline);
-
     grpc::Status status = (stub->SetMetricQuery)(&context, req, &response);
-
     if (!status.ok()) {
       response.set_error_text("Connection lost: " + status.error_message() +
                               "; " + status.error_details());
       response.set_error_code(yagpcc::METRIC_RESPONSE_STATUS_CODE_ERROR);
+      connected = false;
+      cv.notify_one();
     }
 
     return response;
@@ -35,8 +59,27 @@ public:
 
 private:
   const std::string SOCKET_FILE = "unix:///tmp/yagpcc_agent.sock";
-  const std::string TCP_ADDRESS = "127.0.0.1:1432";
   std::unique_ptr<yagpcc::SetQueryInfo::Stub> stub;
+  std::shared_ptr<grpc::Channel> channel;
+  std::atomic_bool connected;
+  std::thread reconnect_thread;
+  std::condition_variable cv;
+  std::mutex mtx;
+  bool done;
+
+  void reconnect() {
+    while (!done) {
+      {
+        std::unique_lock<std::mutex> lock(mtx);
+        cv.wait(lock);
+      }
+      while (!connected && !done) {
+        auto deadline =
+            std::chrono::system_clock::now() + std::chrono::milliseconds(100);
+        connected = channel->WaitForConnected(deadline);
+      }
+    }
+  }
 };
 
 GrpcConnector::GrpcConnector() { impl = new Impl(); }
