e64f4d6f89fdce1626fbfbb4b70fed7e2d500804
Maxim Smyatkin <smyatkinmaxim@gmail.com>
2023-09-06 16:11:06 +0300

    [yagp_hooks_collector] Clean up threading, signal handling, and logging
    Mute PG-destined signals in GRPC reconnection thread.  Move debian
    config to CI.  Redirect debug output to log file.  Harden memory
    handling.  Remove thread-unsafe logging and dead code.

--- src/GrpcConnector.cpp
+++ src/GrpcConnector.cpp
@@ -7,45 +7,72 @@
 #include <grpc++/channel.h>
 #include <grpc++/grpc++.h>
 #include <mutex>
+#include <pthread.h>
+#include <signal.h>
 #include <string>
 #include <thread>
 
-extern "C"
-{
+extern "C" {
 #include "postgres.h"
 #include "cdb/cdbvars.h"
 }
 
-class GrpcConnector::Impl
-{
+/*
+ * Set up the thread signal mask, we don't want to run our signal handlers
+ * in downloading and uploading threads.
+ */
+static void MaskThreadSignals() {
+  sigset_t sigs;
+
+  if (pthread_equal(main_tid, pthread_self())) {
+    ereport(ERROR, (errmsg("thread_mask is called from main thread!")));
+    return;
+  }
+
+  sigemptyset(&sigs);
+
+  /* make our thread to ignore these signals (which should allow that they be
+   * delivered to the main thread) */
+  sigaddset(&sigs, SIGHUP);
+  sigaddset(&sigs, SIGINT);
+  sigaddset(&sigs, SIGTERM);
+  sigaddset(&sigs, SIGALRM);
+  sigaddset(&sigs, SIGUSR1);
+  sigaddset(&sigs, SIGUSR2);
+
+  pthread_sigmask(SIG_BLOCK, &sigs, NULL);
+}
+
+class GrpcConnector::Impl {
 public:
-  Impl() : SOCKET_FILE("unix://" + Config::uds_path())
-  {
+  Impl() : SOCKET_FILE("unix://" + Config::uds_path()) {
     GOOGLE_PROTOBUF_VERIFY_VERSION;
     channel =
         grpc::CreateChannel(SOCKET_FILE, grpc::InsecureChannelCredentials());
     stub = yagpcc::SetQueryInfo::NewStub(channel);
     connected = true;
+    reconnected = false;
     done = false;
     reconnect_thread = std::thread(&Impl::reconnect, this);
   }
 
-  ~Impl()
-  {
+  ~Impl() {
     done = true;
     cv.notify_one();
     reconnect_thread.join();
   }
 
-  yagpcc::MetricResponse set_metric_query(yagpcc::SetQueryReq req)
-  {
+  yagpcc::MetricResponse set_metric_query(const yagpcc::SetQueryReq &req,
+                                          const std::string &event) {
     yagpcc::MetricResponse response;
-    if (!connected)
-    {
+    if (!connected) {
       response.set_error_code(yagpcc::METRIC_RESPONSE_STATUS_CODE_ERROR);
       response.set_error_text(
-          "Not tracing this query connection to agent has been lost");
+          "Not tracing this query because grpc connection has been lost");
       return response;
+    } else if (reconnected) {
+      reconnected = false;
+      ereport(LOG, (errmsg("GRPC connection is restored")));
     }
     grpc::ClientContext context;
     int timeout = Gp_role == GP_ROLE_DISPATCH ? 500 : 250;
@@ -53,12 +80,16 @@ public:
         std::chrono::system_clock::now() + std::chrono::milliseconds(timeout);
     context.set_deadline(deadline);
     grpc::Status status = (stub->SetMetricQuery)(&context, req, &response);
-    if (!status.ok())
-    {
-      response.set_error_text("Connection lost: " + status.error_message() +
-                              "; " + status.error_details());
+    if (!status.ok()) {
+      response.set_error_text("GRPC error: " + status.error_message() + "; " +
+                              status.error_details());
       response.set_error_code(yagpcc::METRIC_RESPONSE_STATUS_CODE_ERROR);
+      ereport(LOG, (errmsg("Query {%d-%d-%d} %s tracing failed with error %s",
+                           req.query_key().tmid(), req.query_key().ssid(),
+                           req.query_key().ccnt(), event.c_str(),
+                           response.error_text().c_str())));
       connected = false;
+      reconnected = false;
       cv.notify_one();
     }
 
@@ -69,25 +100,23 @@ private:
   const std::string SOCKET_FILE;
   std::unique_ptr<yagpcc::SetQueryInfo::Stub> stub;
   std::shared_ptr<grpc::Channel> channel;
-  std::atomic_bool connected;
+  std::atomic_bool connected, reconnected, done;
   std::thread reconnect_thread;
   std::condition_variable cv;
   std::mutex mtx;
-  bool done;
 
-  void reconnect()
-  {
-    while (!done)
-    {
+  void reconnect() {
+    MaskThreadSignals();
+    while (!done) {
       {
         std::unique_lock<std::mutex> lock(mtx);
         cv.wait(lock);
       }
-      while (!connected && !done)
-      {
+      while (!connected && !done) {
         auto deadline =
             std::chrono::system_clock::now() + std::chrono::milliseconds(100);
         connected = channel->WaitForConnected(deadline);
+        reconnected = connected.load();
       }
     }
   }
@@ -98,7 +127,7 @@ GrpcConnector::GrpcConnector() { impl = new Impl(); }
 GrpcConnector::~GrpcConnector() { delete impl; }
 
 yagpcc::MetricResponse
-GrpcConnector::set_metric_query(yagpcc::SetQueryReq req)
-{
-  return impl->set_metric_query(req);
+GrpcConnector::set_metric_query(const yagpcc::SetQueryReq &req,
+                                const std::string &event) {
+  return impl->set_metric_query(req, event);
 }
