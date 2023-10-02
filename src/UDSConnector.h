70acf8d58448d12bebe764fa332712dd1c395d80
Maxim Smyatkin <smyatkinmaxim@gmail.com>
2023-10-02 12:54:32 +0300

    [yagp_hooks_collector] Replace GRPC transport with protobuf-over-UDS
    Remove GRPC dependency.  Serialize metrics as protobuf messages and
    deliver them over a Unix domain socket.  Replace server-side message
    queue with incremental per-query message building.  Add clang-format
    configuration.  Use deprecated protobuf API for bionic compatibility.

---
+++ src/UDSConnector.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "protos/yagpcc_set_service.pb.h"
+#include <queue>
+
+class UDSConnector {
+public:
+  UDSConnector();
+  bool report_query(const yagpcc::SetQueryReq &req, const std::string &event);
+
+private:
+  const std::string uds_path;
+};
