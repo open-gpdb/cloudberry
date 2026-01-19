09e4144eeb16315791f73771d4cf2d83cbbf9ea0
NJrslv <jaroslav.questions@gmail.com>
2026-01-19 11:13:00 +0300

    Add test to CI and --with-yagp-hooks-collector opt
    Change makefile, test and add it to CI of yagp_hooks_collector. Similarly to
    https://github.com/open-gpdb/gpdb/commit/7be8893ef5453e248bd758a47d847e2217f1ae00.

--- devops/build/automation/cloudberry/scripts/configure-cloudberry.sh
+++ devops/build/automation/cloudberry/scripts/configure-cloudberry.sh
@@ -53,6 +53,7 @@
 #
 # Optional Environment Variables:
 #   LOG_DIR - Directory for logs (defaults to ${SRC_DIR}/build-logs)
+#   CONFIGURE_EXTRA_OPTS - Args to pass to configure command
 #   ENABLE_DEBUG - Enable debug build options (true/false, defaults to
 #                  false)
 #
@@ -165,7 +166,8 @@ execute_cmd ./configure --prefix=${BUILD_DESTINATION} \
             --with-openssl \
             --with-uuid=e2fs \
             --with-includes=/usr/local/xerces-c/include \
-            --with-libraries=${BUILD_DESTINATION}/lib || exit 4
+            --with-libraries=${BUILD_DESTINATION}/lib \
+            ${CONFIGURE_EXTRA_OPTS:-""} || exit 4
 log_section_end "Configure"
 
 # Capture version information
