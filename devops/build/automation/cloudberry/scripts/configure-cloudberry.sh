bea3819787fc76d9762971db2a3536489c4a8f35
NJrslv <jaroslav.questions@gmail.com>
2026-03-31 08:34:53 +0300

    [gp_stats_collector] Build by default with extension disabled via GUCs
    Enable building gp_stats_collector by default in configure.  Add
    missing check in verify_query() to ensure the extension does not
    execute main code while disabled.  Always verify protobuf version
    once the shared library is preloaded.

--- devops/build/automation/cloudberry/scripts/configure-cloudberry.sh
+++ devops/build/automation/cloudberry/scripts/configure-cloudberry.sh
@@ -163,6 +163,7 @@ execute_cmd ./configure --prefix=${BUILD_DESTINATION} \
             --disable-pxf \
             --enable-tap-tests \
             ${CONFIGURE_DEBUG_OPTS} \
+            --with-gp-stats-collector \
             --with-gssapi \
             --with-ldap \
             --with-libxml \
